fe4612b62b17841a90d41458a17ec4ab51a16648
mechael7 <mechaelmandausanduo7@gmail.com>
2026-06-23 22:27:03 +0700

    add desain and logic

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const addlisthead = document.getElementById('addListHead');
-const addlistmain = document.querySelectorAll('.add-list');
+const addlistmain = document.getElementById('add-list');
 const list = document.getElementById('list');
-
-// addlisthead.forEach(click => click.addEventListener('click', () => {
-//     if (list.style.display = 'none') {
-//         list.style.display = 'block'
-//     }
-// }))
-// addlistmain.forEach(click => click.addEventListener('click', () => {
-    
-// }))
+const addList = document.getElementById('addList');
+const cancelList = document.getElementById('delete');
+const plusList = document.getElementById('plus');
+const mainList = document.getElementById('main-list');
+const addButton = document.getElementById('list-button');
 
 
 addlisthead.addEventListener('click', () => {
     list.classList.toggle('bang')
+    addList.classList.toggle('hidden')
+});
+plusList.addEventListener('click', () => {
+    list.classList.toggle('bang')
+    addList.classList.toggle('hidden')
     console.log('hello world')
-})
+});
+cancelList.addEventListener('click', () => {
+    list.classList.remove('bang');
+    addList.classList.remove('hidden')
+}); 
 
+addButton.addEventListener('click', () => {
+    const resaultForm = {
+        title : document.getElementById('title-list').value,
+        teks : document.getElementById('large').value
+    }
+    localStorage.setItem('save-title', JSON.stringify(resaultForm));
+});
+
+
+
+// localStorage.clear();
