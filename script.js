cf67653dc496aab1976e4dd154ffdfeb399acfd2
mechael7 <mechaelmandausanduo7@gmail.com>
2026-06-24 21:07:43 +0700

    males

--- script.js
+++ script.js
@@ -6,30 +6,45 @@ const cancelList = document.getElementById('delete');
 const plusList = document.getElementById('plus');
 const mainList = document.getElementById('main-list');
 const addButton = document.getElementById('list-button');
-
+const containerList = document.getElementById('list container')
 
 addlisthead.addEventListener('click', () => {
-    list.classList.toggle('bang')
-    addList.classList.toggle('hidden')
+    list.classList.toggle('bang');
+    addList.classList.toggle('hidden');
 });
 plusList.addEventListener('click', () => {
-    list.classList.toggle('bang')
-    addList.classList.toggle('hidden')
-    console.log('hello world')
+    list.classList.toggle('bang');
+    addList.classList.toggle('hidden');
+    console.log('hello world');
 });
 cancelList.addEventListener('click', () => {
     list.classList.remove('bang');
-    addList.classList.remove('hidden')
+    addList.classList.remove('hidden');
 }); 
 
 addButton.addEventListener('click', () => {
     const resaultForm = {
         title : document.getElementById('title-list').value,
         teks : document.getElementById('large').value
-    }
+    };
     localStorage.setItem('save-title', JSON.stringify(resaultForm));
 });
 
+function writelist () {
+    const getData = JSON.parse(localStorage.getItem('save-title'));
+    if(getData === 0) {
+        addList.classList.remove('hidden');
+    } else {
+        containerList.innerHTML = `
+        <div class="list-note">
+            <div class="note">
+                <h2>
+            </div>
+        </div>`
+    }
+    console.log(getdata);
+}
+writelist()
 
-
+    
 // localStorage.clear();
